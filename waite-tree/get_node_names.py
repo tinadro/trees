96a7f3b9e1249af37071e848697337cb37f670ef
Ms Tina Drobnic <td1515@ls-msc01.bc.ic.ac.uk>
2019-06-04 16:53:40 +0100

    worked on splitting node names to species and gcf. only need to make it work for one exception

--- waite-tree/get_node_names.py
+++ waite-tree/get_node_names.py
@@ -13,8 +13,16 @@ def lookup_by_names(tree):
 tree = Phylo.read('Epsilonbacteraeota.renamed.tree', 'newick')
 names = lookup_by_names(tree)
 
-# key in names.keys():
-#	print(key)
+#def name_split(name):
+
+for node in names:
+	if node.find('GCF_') != -1 or node.find('GCA_') != -1: #if gcf is not provided
+		species = '_'.join(node.split('_', 2)[:2])
+		gcf = '_'.join(node.split('_', 2)[2:])
+	else:
+		species = '_'.join(node.split('_', 2)[:2])
+		uba = '_'.join(node.split('_', 2)[2:])
+		print(uba)
 #this prints out the names very nicely. can save them to a table and then add the 1, 0, -1 for labels accordingly 
 #can also split them to extract the GCF/GCA numbers or uba numbers to get the specific species
 #then would have to check if i have them in my database (manually?) and then check if i found a BBH (full circle), psiblast hit (outline), or not at all (none)
