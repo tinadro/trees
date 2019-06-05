65cbc8b8f902ac3a12d71711572034341b3d5b64
Ms Tina Drobnic <td1515@ls-msc01.bc.ic.ac.uk>
2019-06-05 17:51:52 +0100

    working on cross-referencing assembly codes between waite tree and my bbh results

--- waite-tree/entrez_test.py
+++ waite-tree/entrez_test.py
@@ -8,4 +8,71 @@ record = Entrez.read(handle)
 #get raw assembly summary
 handle = Entrez.esummary(db='assembly', id='1275351', report='full')
 record = Entrez.read(handle)
-print(record)
+#print(type(record))
+
+#print(record['DocumentSummarySet'])
+
+summary = record['DocumentSummarySet']['DocumentSummary']
+
+
+
+#for key in summary[0]['Biosource'].keys():
+#	print(key)
+
+uba = summary[0]['Biosource']['Isolate']
+
+
+#	summary[0] keys:
+#	RsUid
+#	GbUid
+#	AssemblyAccession
+#	LastMajorReleaseAccession
+#	LatestAccession
+#	ChainId
+#	AssemblyName
+#	UCSCName
+#	EnsemblName
+#	Taxid
+#	Organism
+#	SpeciesTaxid
+#	SpeciesName
+#	AssemblyType
+#	AssemblyClass
+#	AssemblyStatus
+#	WGS
+#	GB_BioProjects
+#	GB_Projects
+#	RS_BioProjects
+#	RS_Projects
+#	BioSampleAccn
+#	BioSampleId
+#	Biosource
+#	Coverage
+#	PartialGenomeRepresentation
+#	Primary
+#	AssemblyDescription
+#	ReleaseLevel
+#	ReleaseType
+#	AsmReleaseDate_GenBank
+#	AsmReleaseDate_RefSeq
+#	SeqReleaseDate
+#	AsmUpdateDate
+#	SubmissionDate
+#	LastUpdateDate
+#	SubmitterOrganization
+#	RefSeq_category
+#	AnomalousList
+#	ExclFromRefSeq
+#	PropertyList
+#	FromType
+#	Synonym
+#	ContigN50
+#	ScaffoldN50
+#	FtpPath_GenBank
+#	FtpPath_RefSeq
+#	FtpPath_Assembly_rpt
+#	FtpPath_Stats_rpt
+#	FtpPath_Regions_rpt
+#	SortOrder
+#	Meta
+
--- waite-tree/tree_annotation_PflAB.py
+++ waite-tree/tree_annotation_PflAB.py
@@ -8,8 +8,6 @@ import sys
 
 df = pd.read_csv('species_accession.csv')
 acc = df['accession']
-for a in acc:
-	print(a)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~
 # GET PATH TO RESULTS XLSX
@@ -22,16 +20,16 @@ parentdir = dirname(dirname(dirname(abspath(__file__))))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #extracts the subject_gcf numbers from the PflAB-BBH results tables
-def xls_cleanup(query): # query is PflA or PflB
+def xls_open(query): # query is PflA or PflB
 	bbhresult = parentdir + '/1_bidirectional-best-hits/results-Cj' + query + '.xlsx' # path to results xlsx 
 	xls = pd.ExcelFile(bbhresult) #
 	sheets = xls.sheet_names
-	dic = pd.read_excel(xls, sheet_name=None) # read the file into a dictionary, keys are worksheets and values are dataframes. None means all worksheets are imported 
-	for sheet, df in dic.items():
-		df = df.loc[:, ['saccver', 'subject_gcf']] #keep only the saccvver and gcf columns
-		dic[sheet] = df # save that df as that sheet
-	return dic
-	return sheets
+	dic = pd.read_excel(xls, header=0, sheet_name=None) # read the file into a dictionary, keys are worksheets and values are dataframes. None means all worksheets are imported 
+	del dic[sheets[0]]
+	del dic[sheets[2]]
+	del sheets[0]
+	del sheets[1]
+	return dic, sheets
 # list is 'BBHs', 'BBH-eval-filter', 'psiblast', 'psiblast-eval-filter', 'reidentified-psiblast', 'novel-psiblast'
 
 # find the entrez id associated with input accession number
@@ -45,11 +43,21 @@ def get_entrez_id(accession):
 #fetch raw summary of the assembly identified with the id
 def raw_assembly_summary(as_id):
 	handle = Entrez.esummary(db='assembly', id=as_id, report='full')
-	record = Entrez.read((handle)
+	record = Entrez.read(handle)
 	return record
 
-d_pfla, sheets = xls_cleanup('PflA')
-print(sheets)
+d_pfla, sheets_pfla = xls_open('PflA')
+
+for item in sheets_pfla:
+	for index, line in d_pfla[item].iterrows():
+		gcf = line['subject_gcf']
+		ent_ids = get_entrez_id(gcf)
+		summary = raw_assembly_summary(ent_ids)
+
+
+
+
+d_pflb, sheets_pflb = xls_open('PflB')
 
 
 # iterate over each gcf 
